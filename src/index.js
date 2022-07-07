27bcbf1c58c2e1bf7976dbdcb1cf127004a3ec3f
Jose Simosa Nunez <josecarlossimosa@gmail.com>
2022-07-07 13:36:58 -0500

    commit tree d300ce813ba6f32b9511f73d9489e0b2f9bbf476

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 const init = () => {
-  
+    let form = document.querySelector('form')
+    form.addEventListener('submit',(e) => {
+        e.preventDefault()
+        console.log(e.target.children[1].value)
+    } )
 }
 
 document.addEventListener('DOMContentLoaded', init);
